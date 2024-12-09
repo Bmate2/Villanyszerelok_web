dc5c2193195bf6c3a175b09c695397554f7eed58
UliRobert <ulicsny.robert@gmail.com>
2024-12-09 16:17:26 +0100

    Kosár szépítve

--- lego_webshop/resources/views/cart/index.blade.php
+++ lego_webshop/resources/views/cart/index.blade.php
@@ -13,62 +13,62 @@
 <body>
 @include("layouts.navbar")
 
-<div>
-    <div class="cart-holder">
-        <h1>A kosarad</h1>
-<div class="items">
-        @if (count($cart) > 0)
-            <table>
-                <thead>
-                    <tr>
-                        <th>Termék</th>
-                        <th>DB</th>
-                        <th>Ár</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach ($cart as $item)
-    <tr>
-        <td>{{ $item['name'] }}</td>
-        <td>{{ $item['quantity'] }}</td>
-        <td>{{ $item['price'] * $item['quantity'] }} Ft</td>
-        
-        <td class="action">
-            <form action="{{ route('cart.update', ['product_id' => $item['id'], 'action' => 'decrease']) }}" method="POST">
-                @csrf
-                <button type="submit" style="border: none; background: none; cursor: pointer; color:black;">-</button>
-            </form>
-        </td>
-
-        <td class="action">
-            <form action="{{ route('cart.update', ['product_id' => $item['id'], 'action' => 'increase']) }}" method="POST">
-                @csrf
-                <button type="submit" style="border: none; background: none; cursor: pointer; color:black;">+</button>
-            </form>
-        </td>
-
-        <td class="action">
-            <form action="{{ route('cart.remove', $item['id']) }}" method="POST">
-                @csrf
-                <button type="submit" style="border: none; background: none; cursor: pointer;">🗑️</button>
-            </form>
-        </td>
-    </tr>
-@endforeach
-                </tbody>
-            </table>
-</div>
-            <p>Összesen: {{ array_sum(array_map(function ($item) {
+    <div>
+        <div class="cart-holder">
+            <h1>A kosarad</h1>
+            <div class="items">
+                    @if (count($cart) > 0)
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th class="name">Termék</th>
+                                    <th class="quantity">DB</th>
+                                    <th class="price">Ár</th>
+                                    <th class="actions"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($cart as $item)
+                                    <tr>
+                                        <td class="name">{{ $item['name'] }}</td>
+                                        <td class="quantity">{{ $item['quantity'] }}</td>
+                                        <td class="price">{{ $item['price'] * $item['quantity'] }} Ft</td>
+                                        
+                                        <td class="action">
+                                            <form action="{{ route('cart.update', ['product_id' => $item['id'], 'action' => 'decrease']) }}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="cart-action">➖</button>
+                                            </form>
+                                            <form action="{{ route('cart.update', ['product_id' => $item['id'], 'action' => 'increase']) }}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="cart-action">➕</button>
+                                            </form>
+                                            <form action="{{ route('cart.remove', $item['id']) }}" method="POST">
+                                                @csrf
+                                                <button type="submit" class="cart-action">🗑️</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    <a href="{{ route('aruk') }}"><button type="submit" class="empty-cart-btn">Adj még hozzá termékeket</button></a>
+            </div>
+            <p class="cart-text">Összesen: {{ array_sum(array_map(function ($item) {
                 return $item['price'] * $item['quantity'];
             }, $cart)) }} Ft</p>
-        @else
-            <p>Üres a kosár</p>
+            <hr>
+            <div class="cart-bottom">
+                <form action="{{ route('cart.clear') }}" method="POST">
+                @csrf
+                    <button type="submit" class="idkman">Kosár ürítése</but>
+                </form>
+                <a href="{{ route('aruk') }}"><button type="submit" class="empty-cart-btn">Tovább a rendeléshez</button></a>
+            </div>
+            @else
+                <p class="cart-text">Üres a kosár</p>
+                <a href="{{ route('aruk') }}"><button type="submit" class="empty-cart-btn">Adj hozzá termékeket</button></a>
         @endif
-
-        <form action="{{ route('cart.clear') }}" method="POST">
-            @csrf
-            <button type="submit">Kosár ürítése</button>
-        </form>
     </div>
 </div>
 
